3ca52dbb3c16e78b15c1758f504f927c97bd3aaf
Orlando Zúniga <139894785+orlando-zh@users.noreply.github.com>
2025-11-11 20:46:11 -0600

    refactor: redesign navbar and reorganize script structure for better code order

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(() => {
+    'use strict'
+
+    // ===== Navbar activo =====
+    const links = document.querySelectorAll(".nav-link");
+    if (links.length > 0) {
+        const currentPage = window.location.pathname.split("/").pop();
+        links.forEach(link => {
+            if (link.getAttribute("href") === currentPage) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    }
+
+})();
